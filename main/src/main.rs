04911667f7deb24329ede84387ea27a80b38affd
Andrew Mackenzie <andrew@mackenzie-serres.net>
2019-08-27 14:22:18 +0100

    Convert get_string() to get_bytes() to be more generic

--- main/src/main.rs
+++ main/src/main.rs
@@ -27,14 +27,13 @@ fn load_from_file(filename: &str) -> Module {
 /*
     Allocate memory for a new String inside the wasm module.
 */
-fn new_string(instance: &ModuleRef, memory: &MemoryRef, s: &str) -> u32 {
+fn new_string(instance: &ModuleRef, memory: &MemoryRef, s: &str, bytes: &[u8]) -> u32 {
     let result = instance
         .invoke_export("alloc", &[RuntimeValue::I32((s.len() + 1) as i32)],
                        &mut NopExternals);
 
     match result.unwrap().unwrap() {
         RuntimeValue::I32(pointer) => {
-            let bytes = s.as_bytes();
             let len = bytes.len();
             for i in 0..len {
                 memory.set_value((pointer + i as i32) as u32, bytes[i]).unwrap();
@@ -62,7 +61,7 @@ fn dealloc(instance: &ModuleRef, offset: u32) {
     Sicne the wasm module can only return one result (offset) we have to go through the string
     there until we find the null termination and hence calculate the length
 */
-fn get_string(memory: &MemoryRef, mut offset: u32) -> String {
+fn get_bytes(memory: &MemoryRef, mut offset: u32) -> Vec<u8> {
     let mut bytes: Vec<u8> = vec![];
     loop {
         let mut buf = [0u8; 1];
@@ -78,8 +77,7 @@ fn get_string(memory: &MemoryRef, mut offset: u32) -> String {
             Err(_) => {}
         }
     }
-
-    String::from_utf8(bytes).unwrap()
+    bytes
 }
 
 fn main() {
@@ -100,19 +98,23 @@ fn main() {
         .expect("export name `memory` is not of memory type")
         .to_owned();
 
-    let s2 = "Löwe 老虎 Léopard";
+    let input_data = "What is the meaning of life?";
 
-    let p = new_string(&instance, &memory, s2);
+    // Allocate a string for the input data inside wasm module
+    let input_data_waasm_ptr = new_string(&instance, &memory, input_data, input_data.as_bytes());
+
+    // Run the `run` function on the input_data and get a result back
     let result = instance
-        .invoke_export("run", &[RuntimeValue::I32(p as i32)], &mut NopExternals);
+        .invoke_export("run", &[RuntimeValue::I32(input_data_waasm_ptr as i32)], &mut NopExternals);
 
     match result {
         Ok(e) => {
             match e.unwrap() {
                 RuntimeValue::I32(result_offset) => {
-                    let result = get_string(&memory, result_offset as u32);
+                    let result = get_bytes(&memory, result_offset as u32);
+                    let result_str = String::from_utf8(result).unwrap();
                     dealloc(&instance, result_offset as u32);
-                    println!("Result is `{}`", result);
+                    println!("Result is `{}`", result_str);
                 }
                 _ => println!("Not implemented yet")
             }
